1c5a739efe76ee812005d5c6ce063decf3dd9b28
iachichurov <a.iachichurov@gmail.com>
2022-09-15 03:32:46 +1000

    scoreboard finished

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings():
         self.screen_heigth = 800
         self.background_color = (230, 230, 230)
         # Настройки корабля
-        self.ship_speed = 1.5
+        self.ship_speed = 1.1
         self.ship_limit = 3
         # Параметры снаряда
         self.bullet_speed = 2
@@ -20,11 +20,13 @@ class Settings():
         self.fleet_drop_speed = 10
         # Темп ускорения игры
         self.speedup_scale = 1.1
+        # Темп роста стоимости пришельцев
+        self.score_scale = 1.6
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Инициализирует настройки, изменяющиеся в ходе игры"""
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 1
         self.bullet_speed_factor = 3.0
         self.alien_speed_factor = 0.6
         self.fleet_direction = 1  # 1 обозначает движение вправо, -1 движение влево
@@ -32,7 +34,8 @@ class Settings():
         self.alien_points = 50
 
     def increase_speed(self):
-        """Увеличивает настройки скорости"""
+        """Увеличивает настройки скорости и стоимость пришельцев"""
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
